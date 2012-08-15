e87141b0bd40b7f1b6bac19976608629f6b4cfe7
Henry Harris <henry.harris@gorillalogic.com>
2012-08-14 23:16:26 -0600

    Version 0.0.1
    Setup

--- src/com/newliberty/enderchests/EnderChests.java
+++ src/com/newliberty/enderchests/EnderChests.java
@@ -1,7 +1,22 @@
 package com.newliberty.enderchests;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EnderChests extends JavaPlugin {
+	
+	public void onEnable(){
+	
+	}
+	
+	public void onDisable(){
+		
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		return false;
+	
+	}
 
 }
